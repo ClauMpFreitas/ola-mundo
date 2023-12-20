cd038f8370712c8319ef129754b7bdac88f6e74d
ClauMpFreitas <claudinhampfreitas@gmail.com>
2023-12-19 21:21:43 -0300

    Atualização do projeto

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,24 @@
+import styles from "./Inicio.module.css";
+import posts from "json/posts.json";
+import PostCard from "componentes/PostCard";
+import { Link } from "react-router-dom";
+
+export default function Inicio() {
+  const postsExcluidosIds = [9, 10, 11, 12];
+
+  const postsInicio = posts.filter(
+    (post) => !postsExcluidosIds.includes(post.id)
+  );
+
+  return (
+    <ul className={styles.posts}>
+      {postsInicio.map((post) => (
+        <li key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            <PostCard post={post} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
